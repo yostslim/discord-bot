2ad7f17851b2f05d322406fc4dcf7670f19cc554
yostslim <yostslim@gmail.com>
2026-03-17 15:21:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,17 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
 client.once('ready', () => {
   console.log('Bot connecté !');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong 🏓');
+  }
 });
 
 client.login(process.env.TOKEN);
