a8e7b88697d05d7ba1281087fe3220fcc9db9617
yostslim <yostslim@gmail.com>
2026-03-17 15:08:02 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log('Bot connecté !');
+});
+
+client.login(process.env.TOKEN);
